1d049849b40b9b63d66f6ed15350f1b77fdd777f
csruiliu <rui.liu@proton.me>
2018-07-02 20:05:57 -0500

    working on btree[3]--revise btree node structure

--- src/db_index_btree.c
+++ src/db_index_btree.c
@@ -5,6 +5,7 @@
 #include <utils.h>
 #include <db_manager.h>
 #include <string.h>
+
 #include "db_index_btree.h"
 
 BTree* btree_init() {
@@ -13,91 +14,91 @@ BTree* btree_init() {
         log_err("[%d] init btree failed!\n");
         return NULL;
     }
-    btree->key_num = 0;
+    btree->kv_num = 0;
     btree->leaf = true;
-    btree->max_key = -1;
-    btree->min_key = -1;
     return btree;
 }
 
 
-BTree* btree_insert(BTree* btree, type_t key) {
-    if(btree->key_num == MAX_KEY_NUM) {
+BTree* btree_insert(BTree* btree, int rowId, int value) {
+    if(btree->kv_num == MAX_KV_NUM) {
 
     }
-    return btree_insert_notfull(btree, key);
+    return btree_insert_notfull(btree, rowId, value);
 }
 
-BTree* btree_delete(BTree* btree, type_t key) {
+BTree* btree_delete(BTree* btree, int rowId, int value) {
     return NULL;
 }
 
-BTree* btree_insert_notfull(BTree* btree, type_t key) {
+/**
+ * insert key into a btree node which is not full
+ **/
+BTree* btree_insert_notfull(BTree* btree_n, int rowId, int value) {
     //the tree is a leaf node
-    if(btree->leaf) {
-        if(btree->max_key == -1 && btree->min_key == -1) {
-            btree->max_key = key;
-            btree->min_key = key;
-            btree->key[0] = key;
-            btree->key_num = 1;
+    if(btree_n->leaf) {
+        if(value >= btree_n->kvs[btree_n->kv_num-1].value) {
+            btree_n->kvs[btree_n->kv_num].value = value;
+            btree_n->kvs[btree_n->kv_num].rowId = rowId;
+            btree_n->kv_num++;
+        }
+        else if (value <= btree_n->kvs[0].value) {
+            for(int i = btree_n->kv_num-1; i >= 0; --i) {
+                btree_n->kvs[i+1] = btree_n->kvs[i];
+            }
+            btree_n->kvs[0].value = value;
+            btree_n->kvs[0].rowId = rowId;
+            btree_n->kv_num++;
         }
         else {
-            if(key >= btree->max_key) {
-                btree->key[btree->key_num] = key;
-                btree->key_num++;
-                btree->max_key = key;
-            }
-            else if(key <= btree->min_key) {
-                for(int i = btree->key_num; i > 0; --i) {
-                    btree->key[i] = btree->key[i-1];
+            for(int j = btree_n->kv_num-1; j > 0; --j) {
+                if(value <= btree_n->kvs[j].value && value > btree_n->kvs[j-1].value) {
+                    btree_n->kvs[j+1] = btree_n->kvs[j];
+                    btree_n->kvs[j].value = value;
+                    btree_n->kvs[j].rowId = rowId;
+                    btree_n->kv_num++;
+                    break;
                 }
-                btree->key[0] = key;
-                btree->min_key = key;
-            }
-            else {
-                //we have min_key, so don't need to consider j = 0
-                for(int j = btree->key_num-1; j > 0; --j) {
-                    btree->key[j+1] = btree->key[j];
-                    if(key < btree->key[j] && key > btree->key[j-1]) {
-                        btree->key[j] = key;
-                        break;
-                    }
+                else {
+                    btree_n->kvs[j+1] = btree_n->kvs[j];
                 }
             }
         }
     }
-    //the tree is not a leaf node
+    //the tree node is not a leaf node, then we must insert key into the appropriate leaf node
     else {
-        if (key < btree->min_key) {
-            if(btree->child[0]->key_num == MAX_KEY_NUM) {
-                btree_split_child(btree, 0, btree->child[0]);
+        if(value >= btree_n->kvs[btree_n->kv_num-1].value) {
+            //since the b-tree is built from bottom to top, is the parent is full then means all the regarding children are full, so split
+            if(btree_n->child[MAX_CHILD_NUM-1]->kv_num == MAX_KV_NUM) {
+                btree_split_child(btree_n,MAX_CHILD_NUM-1,btree_n->child[MAX_CHILD_NUM-1]);
+                if(value > btree_n->kvs[btree_n->kv_num-1].value) {
+                    btree_insert_notfull(btree_n->child[btree_n->kv_num],rowId,value);
+                }
+                else {
+                    btree_insert_notfull(btree_n->child[btree_n->kv_num-1],rowId,value);
+                }
             }
             else {
-                btree_insert_notfull(btree->child[0], key);
+                btree_insert_notfull(btree_n->child[MAX_CHILD_NUM-1],rowId,value);
             }
         }
-        else if (key > btree->max_key) {
-            if(btree->child[btree->key_num]->key_num == MAX_KEY_NUM) {
-                btree_split_child(btree, 0, btree->child[0]);
-            }
-            else {
-                btree_insert_notfull(btree->child[btree->key_num], key);
-            }
+        else if(value <= btree_n->kvs[0].value) {
+            btree_insert_notfull(btree_n->child[0],rowId,value);
         }
         else {
-            for(int k = btree->key_num - 1; k > 0; --k) {
-                if(key < btree->key[k] && key > btree->key[k-1]) {
-                    btree_insert_notfull(btree->child[k], key);
+            for(int i = btree_n->kv_num-1; i > 0; --i) {
+                if(value <= btree_n->kvs[i].value && value > btree_n->kvs[i-1].value) {
+                    btree_insert_notfull(btree_n->child[i],rowId,value);
                     break;
                 }
             }
         }
     }
-    return btree;
+    return btree_n;
 }
 
 /**
- * split the pos-th child of the parent, parent is nonfull and child is full
+ * split the pos-th child of the parent, parent is non-full and child is full
  * the larger half is added to new allocated child
  **/
 BTree* btree_split_child(BTree* parent, int pos, BTree* child) {
@@ -110,11 +111,12 @@ BTree* btree_split_child(BTree* parent, int pos, BTree* child) {
 
     //copy last T-1 items to new child
     for(int i = 0; i <= T-2; ++i) {
-        new_child->key[i] = child->key[i+T];
-        child->key[i+T] = 0;
+        new_child->kvs[i] = child->kvs[i+T];
+        child->kvs[i+T].value = 0;
+        child->kvs[i+T].rowId = -1;
     }
-    new_child->key_num = T-1;
-    child->key_num = T-1;
+    new_child->kv_num = T-1;
+    child->kv_num = T-1;
     //if child is leaf node, also need to copy regarding children
     if(child->leaf == false) {
         for(int i = MAX_CHILD_NUM-1; i <= T; --i) {
@@ -122,16 +124,16 @@ BTree* btree_split_child(BTree* parent, int pos, BTree* child) {
             child->child[i] = NULL;
         }
     }
-    //move parents keys for new median value
-    for(int j = parent->key_num-1; j >= pos; --j) {
-        parent->key[j+1] = parent->key[j];
+    //move parents kv for new median value
+    for(int j = parent->kv_num-1; j >= pos; --j) {
+        parent->kvs[j+1] = parent->kvs[j];
     }
     //move parents children for new median value
-    for(int k = parent->key_num; k > pos; --k) {
+    for(int k = parent->kv_num; k > pos; --k) {
         parent->child[k+1] = parent->child[k];
     }
-    parent->key[pos] = child->key[T];
-    parent->key_num++;
+    parent->kvs[pos] = child->kvs[T];
+    parent->kv_num++;
     parent->child[pos] = child;
     parent->child[pos+1] = new_child;
 }
--- src/include/db_index_btree.h
+++ src/include/db_index_btree.h
@@ -6,29 +6,40 @@
 #define COLDB_DB_INDEX_H
 
 #define T 127
-#define MAX_KEY_NUM 2 * T - 1
-#define MAX_CHILD_NUM MAX_KEY_NUM + 1
+#define MAX_KV_NUM 2 * T - 1
+#define MAX_CHILD_NUM MAX_KV_NUM + 1
+
+#include <stdbool.h>
 
 typedef int type_t;
 
-typedef struct btree_t {
-    int key_num;                   /* number of keys in this node*/
-    type_t max_key;                /* max key in this subtree */
-    type_t min_key;                /* min key in this subtree */
-    bool leaf;                     /* it is a leaf node or not, '1' is leaf, '0' is not */
-    type_t key[MAX_KEY_NUM];
-    struct btree_t* child[MAX_CHILD_NUM]; /* subtrees of this tree*/
+/**
+ * only consider int type so far, so the btree_kv are only int
+ **/
+typedef struct btree_kv {
+    type_t rowId;
+    type_t value;
+} BTree_KV;
 
-} BTree, BTNode;
+typedef struct btree_t {
+    unsigned int kv_num;                   /* number of keys in this node */
+    bool leaf;                     /* it is a leaf node or not, '1' is leaf, '0' is not */
+    BTree_KV kvs[MAX_KV_NUM];
+    struct btree_t* child[MAX_CHILD_NUM]; /* subtrees of this tree*/
+} BTree;
 
 BTree* btree_init();
 
-BTree* btree_insert(BTree* btree, type_t key);
+BTree* btree_insert(BTree* btree_n, int rowId, int value);
 
-BTree* btree_delete(BTree* btree, type_t key);
+BTree* btree_delete(BTree* btree_n, int rowId, int value);
+
+BTree* btree_delete_by_rowId(BTree* btree_n, int rowId, int value);
+
+BTree* btree_delete_by_value(BTree* btree_n, int rowId, int value);
 
 BTree* btree_split_child(BTree* parent, int pos, BTree* child);
 
-BTree* btree_insert_notfull(BTree* btree, type_t key);
+BTree* btree_insert_notfull(BTree* btree_n, int rowId, int value);
 
 #endif //COLDB_DB_INDEX_H

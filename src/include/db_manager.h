0b97f4320f5222cd07e322ca688cc257531c80e8
csruiliu <rui.liu@proton.me>
2021-01-25 17:19:39 -0600

    update db_manager[1]

--- src/include/db_manager.h
+++ src/include/db_manager.h
@@ -8,23 +8,56 @@
 #include "db_fds.h"
 #include "parse.h"
 
+/**
+ *  create db, table, column
+ **/
 Db* create_db(char* db_name);
 
 Table* create_table(char* db_name, char* tbl_name, char* pricls_col_name, size_t num_columns);
 
 Column* create_column(char* tbl_name, char* col_name);
 
-int load_data_csv(char* data_path);
-
-int persist_data_csv();
-
+/**
+ *  prepare database after setup
+ **/
 int setup_db_csv();
 
-int set_column_idx_cls(Column* slcol, char* idx_type, char* cls_type);
+/**
+ *  load/save data from/to disk using CVS format
+ **/
+int load_data_csv(char* data_path);
+
+int save_data_csv();
 
 int insert_data_col(Column* col, int data, int rowId);
 
 int insert_data_tbl(Table* itbl, int* row_values);
+
+
+
+int set_column_idx_cls(Column* slcol, char* idx_type, char* cls_type);
+
+
+int select_data_col_unidx(Column* scol, char* handle, char* pre_range, char* post_range);
+
+int fetch_col_data(char* col_val_name, char* rsl_vec_pos, char* handle);
+
+int avg_col_data(char* avg_name, char* handle);
+
+int sum_col_data(char* sum_name, char* handle);
+
+int add_col_col(char* add_name1, char* add_name2, char* handle);
+
+int add_col_rsl(char* add_name1, char* add_name2, char* handle);
+
+int sub_col_col(char* sub_name1, char* sub_name2, char* handle);
+
+
+
+int set_column_idx_cls(Column* slcol, char* idx_type, char* cls_type);
+
+
+
 
 int select_data_col_unidx(Column* scol, char* handle, char* pre_range, char* post_range);
 

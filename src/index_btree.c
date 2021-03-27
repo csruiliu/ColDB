dcf80d4c4428b13896439e8e369663b70f030bec
csruiliu <rui.liu@gmx.com>
2021-03-27 09:53:21 -0500

    revise btree index

--- src/index_btree.c
+++ src/index_btree.c
@@ -1,159 +1,178 @@
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "utils_func.h"
 #include "index_btree.h"
 
-BTree* btree_init() {
-    BTree* btree = (BTree*) malloc(sizeof(BTree));
-    if(btree == NULL) {
-        log_err("[%d] init btree failed!\n");
-        return NULL;
-    }
-    btree->kv_num = 0;
-    btree->leaf = true;
-    return btree;
+#define MAX_KEYS (16)
+
+struct bt_node {
+    //is this a leaf node? 1 is a leaf node, 0 is not a leaf node
+    int is_leaf;
+    //how many keys does this node contain
+    int num_keys;
+    // the keys in the node
+    int keys[MAX_KEYS];
+    //kids[i] holds nodes < keys[i]
+    struct bt_node* kids[MAX_KEYS+1];
+};
+
+btree btree_init() {
+    btree b;
+    b = malloc(sizeof(*b));
+    assert(b);
+    b->is_leaf = 1;
+    b->num_keys = 0;
+
+    return b;
 }
 
-BTree* btree_insert(BTree* btree, int rowId, int value) {
-    if(btree->kv_num == MAX_KV_NUM) {
-
+void btree_destroy(btree t) {
+    size_t i;
+    if (!t->is_leaf) {
+        for(i = 0; i < t->num_keys+1; ++i) {
+            btree_destroy(t->kids[i]);
+        }
     }
-    return btree_insert_notfull(btree, rowId, value);
+
+    free(t);
 }
 
 /**
- * insert key into a btree node which is not full
- **/
-BTree* btree_insert_notfull(BTree* btree_n, int rowId, int value) {
-    //the tree is a leaf node
-    if(btree_n->leaf) {
-        if(value >= btree_n->kvs[btree_n->kv_num-1].value) {
-            btree_n->kvs[btree_n->kv_num].value = value;
-            btree_n->kvs[btree_n->kv_num].rowId = rowId;
-            btree_n->kv_num++;
+ * search key in a tree
+ */
+static int search_key(int n, const int *a, int key) {
+    int low;
+    int high;
+    int mid;
+
+    low = -1;
+    high = n;
+
+    while(low + 1 < high) {
+        mid = (low + high) / 2;
+        if (a[mid] == key) {
+            return mid;
         }
-        else if (value <= btree_n->kvs[0].value) {
-            for(size_t i = btree_n->kv_num-1; i >= 0; --i) {
-                btree_n->kvs[i+1] = btree_n->kvs[i];
-            }
-            btree_n->kvs[0].value = value;
-            btree_n->kvs[0].rowId = rowId;
-            btree_n->kv_num++;
+        else if (a[mid] < key) {
+            low = mid;
         }
         else {
-            for(size_t j = btree_n->kv_num-1; j > 0; --j) {
-                if(value <= btree_n->kvs[j].value && value > btree_n->kvs[j-1].value) {
-                    btree_n->kvs[j+1] = btree_n->kvs[j];
-                    btree_n->kvs[j].value = value;
-                    btree_n->kvs[j].rowId = rowId;
-                    btree_n->kv_num++;
-                    break;
-                }
-                else {
-                    btree_n->kvs[j+1] = btree_n->kvs[j];
-                }
-            }
+            high = mid;
         }
     }
-    //the tree node is not a leaf node, then we must insert key into the appropriate leaf node
+
+    return high;
+}
+
+int btree_search(btree t, int key) {
+    int pos;
+
+    // have to check for empty tree
+    if (t->num_keys == 0) {
+        return 0;
+    }
+
+    pos = search_key(t->num_keys, t->keys, key);
+
+    if(pos < t->num_keys && t->keys[pos] == key) {
+        return 1;
+    }
     else {
-        if(value >= btree_n->kvs[btree_n->kv_num-1].value) {
-            //since the b-tree is built from bottom to top, is the parent is full then means all the regarding children are full, so split
-            if(btree_n->child[MAX_CHILD_NUM-1]->kv_num == MAX_KV_NUM) {
-                btree_split_child(btree_n,MAX_CHILD_NUM-1,btree_n->child[MAX_CHILD_NUM-1]);
-                if(value > btree_n->kvs[btree_n->kv_num-1].value) {
-                    btree_insert_notfull(btree_n->child[btree_n->kv_num],rowId,value);
-                }
-                else {
-                    btree_insert_notfull(btree_n->child[btree_n->kv_num-1],rowId,value);
-                }
-            }
-            else {
-                btree_insert_notfull(btree_n->child[MAX_CHILD_NUM-1],rowId,value);
-            }
-        }
-        else if(value <= btree_n->kvs[0].value) {
-            if(btree_n->child[0]->kv_num == MAX_KV_NUM) {
-                btree_split_child(btree_n,0,btree_n->child[0]);
-                if(value > btree_n->kvs[0].value) {
-                    btree_insert_notfull(btree_n->child[1],rowId,value);
-                }
-                else {
-                    btree_insert_notfull(btree_n->child[0],rowId,value);
-                }
-            }
-            else {
-                btree_insert_notfull(btree_n->child[0],rowId,value);
-            }
-        }
-        else {
-            for(size_t i = btree_n->kv_num-1; i > 0; --i) {
-                if(value <= btree_n->kvs[i].value && value > btree_n->kvs[i-1].value) {
-                    if(btree_n->child[i]->kv_num == MAX_KV_NUM) {
-                        btree_split_child(btree_n, i, btree_n->child[i]);
-                        if(value > btree_n->kvs[i].value) {
-                            btree_insert_notfull(btree_n->child[i+1],rowId,value);
-                        }
-                        else {
-                            btree_insert_notfull(btree_n->child[i],rowId,value);
-                        }
-                    }
-                    else {
-                        btree_insert_notfull(btree_n->child[i],rowId,value);
-                    }
-                    break;
-                }
-            }
-        }
+        return (!t->is_leaf && btree_search(t->kids[pos], key));
     }
-    return btree_n;
 }
 
 /**
- * split the pos-th child of the parent, parent is non-full and child is full
- * the larger half is added to new allocated child
- **/
-BTree* btree_split_child(BTree* parent, size_t pos, BTree* child) {
-    BTree* new_child = (BTree*) malloc(sizeof(BTree));
-    if(new_child == NULL) {
-        log_err("init new btree for splitting failed!\n");
-        return NULL;
-    }
-    new_child->leaf = child->leaf;
+ * insert a new key into a tree
+ * returns new right sibling if the node splits
+ * and puts the median in *median
+ * else returns 0
+ */
 
-    //copy last T-1 items to new child
-    for(int i = 0; i <= T-2; ++i) {
-        new_child->kvs[i] = child->kvs[i+T];
-        child->kvs[i+T].value = 0;
-        child->kvs[i+T].rowId = -1;
+static btree btree_insert_internal(btree t, int key, int *median) {
+    int pos;
+    int mid;
+    btree b_new;
+
+    pos = search_key(t->num_keys, t->keys, key);
+
+    if(pos < t->num_keys && t->keys[pos] == key) {
+        return 0;
     }
-    new_child->kv_num = T-1;
-    child->kv_num = T-1;
-    //if child is leaf node, also need to copy regarding children
-    if(child->leaf == false) {
-        for(int i = MAX_CHILD_NUM-1; i <= T; --i) {
-            new_child->child[i-T] = child->child[i];
-            child->child[i] = NULL;
+
+    if (t->is_leaf) {
+        // every key above pos moves up one space
+        memmove(&t->keys[pos+1], &t->keys[pos], sizeof(*(t->keys)) * (t->num_keys - pos));
+        t->keys[pos] = key;
+        t->num_keys++;
+    }
+    else {
+        b_new = btree_insert_internal(t->kids[pos], key, &mid);
+
+        if (b_new) {
+            //every key above pos moves up one space
+            memmove(&t->keys[pos+1], &t->keys[pos], sizeof(*(t->keys)) * (t->num_keys - pos));
+            //new kid goes in pos + 1
+            memmove(&t->kids[pos+2], &t->kids[pos+1], sizeof(*(t->keys)) * (t->num_keys - pos));
+
+            t->keys[pos] = mid;
+            t->kids[pos+1] = b_new;
+            t->num_keys++;
         }
     }
-    //move parents kv for new median value
-    for(size_t j = parent->kv_num-1; j >= pos; --j) {
-        parent->kvs[j+1] = parent->kvs[j];
+
+    //we waste a tiny bit of space by splitting now instead of on next insert
+    if(t->num_keys >= MAX_KEYS) {
+        mid = t->num_keys / 2;
+        *median = t->keys[mid];
+        b_new = malloc(sizeof(*b_new));
+        b_new->num_keys = t->num_keys - mid - 1;
+        b_new->is_leaf = t->is_leaf;
+        memmove(b_new->keys, &t->keys[mid+1], sizeof(*(t->keys)) * b_new->num_keys);
+
+        if(!t->is_leaf) {
+            memmove(b_new->kids, &t->kids[mid+1], sizeof(*(t->kids)) * (b_new->num_keys + 1));
+        }
+        t->num_keys = mid;
+        return b_new;
     }
-    //move parents children for new median value
-    for(size_t k = parent->kv_num; k > pos; --k) {
-        parent->child[k+1] = parent->child[k];
+    else {
+        return 0;
     }
-    parent->kvs[pos] = child->kvs[T];
-    parent->kv_num++;
-    parent->child[pos] = child;
-    parent->child[pos+1] = new_child;
-    return new_child;
 }
 
+void btree_insert(btree t, int key) {
+    btree b_left;
+    btree b_right;
+    int median;
 
+    b_right = btree_insert_internal(t, key, &median);
 
+    if (b_right) {
+        //basic issue here is that we are at the root, so if we split, we have to make a new root
+        b_left = malloc(sizeof (*b_left));
+        assert(b_left);
 
+        //copy root to b_left
+        memmove(b_left, t, sizeof(*t));
 
+        //make root point to b_left and b_right
+        t->num_keys = 1;
+        t->is_leaf = 0;
+        t->keys[0] = median;
+        t->kids[0] = b_left;
+        t->kids[1] = b_right;
+    }
+}
 
+void btree_inorder_traversal(btree t) {
+    if (t != NULL) {
+        btree_inorder_traversal(t->kids[0]);
+        for(size_t i = 0; i < t->num_keys; ++i) {
+            printf("%d", t->keys[i]);
+        }
+        btree_inorder_traversal(t->kids[1]);
+    }
+}
